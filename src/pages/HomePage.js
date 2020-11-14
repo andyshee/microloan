62b5847b209e877e9e745763af255dc23abb5251
josh-lind <lind.98@osu.edu>
2020-11-13 23:02:48 -0500

    home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,12 @@
-class Welcome extends React.Component {
+import React from 'react';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
     render() {
         return <Container>
             {/* Stack the columns on mobile by making one full-width and the other half-width */}
