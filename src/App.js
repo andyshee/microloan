0c7686d5141ec3f87eabc29973d25f6703267ed5
Connor Boham <connor@Connors-MacBook-Pro.local>
2020-11-14 01:06:55 -0500

    tried to connect shit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -7,34 +8,51 @@ import {
   Link
 } from "react-router-dom"
 import HomePage from './pages/HomePage';
-
+import api from './api/api';
 //page imports
 import Login from './pages/login'
 import Home from './pages/HomePage'
 import MyNavbar from './components/navbar';
 
-function App() {
-  return (
-    <Router>
-      <MyNavbar />
-      {/* A <Switch> looks through its children <Route>s and
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    api.get('')
+      .then(res => {
+        const text = res.data;
+        console.log(text);
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return (
+      <Router>
+        <MyNavbar />
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/messages">
-          <Messages />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/messages">
+            <Messages />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router >
+    );
+  }
+
 }
 
 function Profile() {
@@ -45,4 +63,3 @@ function Messages() {
   return <p>messages</p>
 }
 
-export default App;
