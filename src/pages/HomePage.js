9651cc29c5e4d56904674020464e762407ad2872
josh-lind <lind.98@osu.edu>
2020-11-14 12:57:37 -0500

    more stuff

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Listitem from '../components/listitem'
 import Detaileditem from '../components/detaileditem'
 import './HomePage.css'
+import FeedItem from '../components/FeedItem';
 
 export default class HomePage extends React.Component {
     constructor(props) {
@@ -13,9 +11,34 @@ export default class HomePage extends React.Component {
             posts: []
         }
     }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({posts: this.getFakePosts()});
+        }, 300);
+    }
+
+    getFakePosts() {
+        const posts = [];
+        for (let i = 1; i < 12; i++) {
+            const p = {
+                id: i,
+                name: 'John Doe',
+                title: 'Franklin Food Pantry',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                targetDate: new Date(),
+                activityLength: 4.5
+            };
+            posts.push(p);
+        }
+        return posts;
+    }
+
     render() {
         return <div className="home-container">
-            <div className="sidebar scrolly"><Listitem /></div>
+            <div className="sidebar scrolly">
+                {this.state.posts.map(p => <FeedItem post={p} />)}
+            </div>
             <div className="maincontent scrolly"><Detaileditem /></div>
         </div>;
     }
