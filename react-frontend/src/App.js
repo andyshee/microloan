7aa2b681ac49e17d936b73409ecf1ee622a77f0f
Andy Shee <andyshee@Andys-MacBook-Pro.local>
2020-11-13 22:21:56 -0500

    google auth working

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+import config from "./firebaseConfig"
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+var firebaseui = require('firebaseui');
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  // Other config options...
+});
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +40,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div id="firebaseui-auth-container">
+
+      </div>
     </div>
   );
 }
---
+++ react-frontend/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyCbd7MXZ3S5qs2XZyuRQm1tQc05IbKGJoo",
+    authDomain: "microloan-94e1d.firebaseapp.com",
+    databaseURL: "https://microloan-94e1d.firebaseio.com",
+    projectId: "microloan-94e1d",
+    storageBucket: "microloan-94e1d.appspot.com",
+    messagingSenderId: "317108792590",
+    appId: "1:317108792590:web:8b0a2b96722bcadb4c3b5f",
+    measurementId: "G-M3HQ4KB6X0"
+  };
+
+  export default config;
